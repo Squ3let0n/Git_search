98a917ba46cc0a8954e6d35f9c3a27fedd125139
Squ3let0n <83617533+Squ3let0n@users.noreply.github.com>
2022-02-20 12:30:19 -0300

    Add files via upload

---
+++ github/app.js
@@ -0,0 +1,12 @@
+const API_LINK = 'https://api.github.com/user'
+
+let search_btn = document.querySelector('.pesquisar')
+
+let search_term = document.querySelector('#search-term')
+
+console.log(search_btn)
+
+search_term = document.querySelector('')
+
+search_btn.addEventListener("click", (e) => {
+	alert("clicked element"))};
